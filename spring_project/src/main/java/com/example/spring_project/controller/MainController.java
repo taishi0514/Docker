7f458663857f16dfb6c88e7d4c2a0f15300f389a
taishi0514 <notaishi4@gmail.com>
2023-10-01 23:29:09 +0900

    書籍表示についての処理 #13

--- spring_project/src/main/java/com/example/spring_project/controller/MainController.java
+++ spring_project/src/main/java/com/example/spring_project/controller/MainController.java
@@ -1,18 +1,35 @@
 package com.example.spring_project.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.spring_project.constant.UrlConst;
+import com.example.spring_project.entity.BookInfo;
+import com.example.spring_project.form.BookForm;
+import com.example.spring_project.service.BookService;
+
+import org.springframework.ui.Model;
+
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class MainController {
 
+	private final BookService service;
+
     @GetMapping(UrlConst.MAIN)
-	public String view() {
+	public String view(Model model, BookForm form) {
+
+		List<BookInfo> allBooks = service.getAllBooks();
+		model.addAttribute("books", allBooks);
 
 		return "main";
 	}
+
 
     
 }
